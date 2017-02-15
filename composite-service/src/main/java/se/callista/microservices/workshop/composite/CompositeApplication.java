402abe9528e4ba3efcf08fd5f2537e87aea2b228
Your Name <you@example.com>
2017-02-15 09:19:20 +0000

    3 services

---
+++ composite-service/src/main/java/se/callista/microservices/workshop/composite/CompositeApplication.java
@@ -0,0 +1,12 @@
+package se.callista.microservices.workshop.composite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompositeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompositeApplication.class, args);
+	}
+}
